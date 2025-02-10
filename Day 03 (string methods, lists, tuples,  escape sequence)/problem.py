06632618324426c5ef8adbdfc0592d812e18adef
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2025-02-10 22:18:17 +0500

    day 03 done with problems

---
+++ Day 03 (string methods, lists, tuples,  escape sequence)/problem.py
@@ -0,0 +1,82 @@
+# # # problem 1, take input from the user and store it in list
+
+# # # fruit = []
+# # # inputs = input("Enter fruit name: ")
+# # # fruit.append(inputs)
+# # # print("Your List of fruits are currently this : \n",fruit)
+
+# # # input2 = input("Enter fruit name: ")
+# # # fruit.append(input2)
+# # # print("Your List of fruits are currently this : \n",fruit)
+
+# # # input3 = input("Enter fruit name: ")
+# # # fruit.append(input3)
+# # # print("Your List of fruits are currently this : \n",fruit)
+
+# # # input4 = input("Enter fruit name: ")
+# # # fruit.append(input4)
+# # # print("Your List of fruits are currently this : \n",fruit)
+
+
+# # # Problem 2, take input from the user of 6 student marks and store them in sorted list
+
+# # marks = []
+
+# # takeInput = int(input("Enter marks of student: "))
+# # marks.append(takeInput)
+
+# # takeInput2 = int(input("Enter marks of student: "))
+# # marks.append(takeInput2)
+
+# # takeInput3 = int(input("Enter marks of student: "))
+# # marks.append(takeInput3)
+
+# # takeInput4 = int(input("Enter marks of student: "))
+# # marks.append(takeInput4)
+
+# # takeInput5 = int(input("Enter marks of student: "))
+# # marks.append(takeInput5)
+
+# # takeInput6 = int(input("Enter marks of student: "))
+# # marks.append(takeInput6)
+
+# # marks.sort()
+# # print("Your sorted list of marks is: \n",marks)
+
+
+# # problem 3, check the changing of element in list and tuple
+
+# # list = ["Furit", "Dream", 34, 90]
+# # list[0] = "Apple"
+# # print(list) # List will change
+
+# # tuple = ("Furit", "Dream", 34, 90)
+# # tuple[0] = "Apple"
+# # print(tuple) # Tuple will not change
+
+
+# # problem 4, write a program take sum the list of 4 numbers
+
+# list = []
+
+# takeInput = int(input("Enter number: "))
+# list.append(takeInput)
+# takeInput2 = int(input("Enter number: "))
+# list.append(takeInput2)
+# takeInput3 = int(input("Enter number: "))
+# list.append(takeInput3)
+# takeInput4 = int(input("Enter number: "))
+# list.append(takeInput4)
+
+
+# totalSum = sum(list) # how the fug its a builtin function in python to sum the arrayyy/list
+# print("Your Total is : ", totalSum)
+
+
+# problem 5, write a program to count a total number of elements in a tuple
+
+tuple = (6, 7, 7,5,2 ,2, 4,5,6)
+
+countedTuple = tuple.count(7)
+
+print(countedTuple)
--- Day 03 (string methods, lists, tuples,  escape sequence)/tuples.py
+++ Day 03 (string methods, lists, tuples,  escape sequence)/tuples.py
@@ -7,17 +7,17 @@ tuple2 = ("Hello", "World")
 concatTuple = tuple + tuple2
 print(concatTuple)
 
-# no = tuple.count("Samad")
-# # print(no)
+no = tuple.count("Samad")
+# print(no)
 
-# falseNo = tuple.count(3)
-# print(falseNo)
+falseNo = tuple.count(3)
+print(falseNo)
 
-# index = tuple.index(True)
-# print(index)
+index = tuple.index(True)
+print(index)
 
-# falseIndex = tuple.index("hello")
-# print(falseIndex)
+falseIndex = tuple.index("hello")
+print(falseIndex)
 
 print(2 in concatTuple) # True or False
 print(100 in concatTuple) # True or False
