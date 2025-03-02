e8b6963ee1785532b79686fc69e9175b429b6156
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2025-03-02 19:48:48 +0500

    new projet added

---
+++ Project 2_Number_Guessing_Game/main.py
@@ -0,0 +1,34 @@
+import random
+
+
+def game():
+    random_Number = random.randint(1, 10)
+    total_Guesses = 1
+
+    print("\nWelcome to the Number Guessing Game!\n")
+
+    while True:
+        try:
+            userInput = int(input("Guess a number between 1 and 10: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+    while userInput != random_Number:
+        if userInput < random_Number:
+            print("Guess higher!\n")
+        else:
+            print("Guess lower!\n")
+
+        while True:
+            try:
+                userInput = int(input("Guess again: "))
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+        total_Guesses += 1
+    print("You guessed it! The number was:", random_Number)
+    print("Your total guesses were:", total_Guesses)
+
+
+game()
