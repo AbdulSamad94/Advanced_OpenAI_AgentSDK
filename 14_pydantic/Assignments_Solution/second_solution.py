c9889d78a593c86f2f9e6413810a872c0c308a3d
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2025-05-04 01:07:46 +0500

    pydantic solution 2 done

---
+++ 14_pydantic/Assignments_Solution/second_solution.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class Employee(BaseModel):
+    id: int
+    name: str = Field(
+        ...,
+        min_length=3,
+        max_length=50,
+        description="Name of the employee",
+        example="Abdul Samad",
+    )
+    department: Optional[str] = "General"
+    salary: float = Field(
+        ...,
+        ge=100000,
+    )
+
+
+example_input = {
+    "id": 1,
+    "name": "Abdul Samad",
+    "department": "General",
+    "salary": 1200000,
+}
+
+employee = Employee(**example_input)
+print(employee)
