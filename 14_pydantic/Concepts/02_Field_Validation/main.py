8c5f3efa5a1dcaff35954d282f004a2c844ca41a
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2025-05-07 23:47:19 +0500

    updated

--- 14_pydantic/Concepts/02_Field_Validation/main.py
+++ 14_pydantic/Concepts/02_Field_Validation/main.py
@@ -1,14 +1,27 @@
-from pydantic import BaseModel, Field  # type: ignore
-from typing import List, Dict, Optional
+# from pydantic import BaseModel, Field  # type: ignore
+# from typing import List, Dict, Optional
 
 
-class Cart(BaseModel):
-    user_id: int = Field(..., gt=0, description="User ID")
-    items: List[str]
-    quantities: Dict[str, int]
+# class Cart(BaseModel):
+#     user_id: int = Field(..., gt=0, description="User ID")
+#     items: List[str]
+#     quantities: Dict[str, int]
 
 
-class BlogPost(BaseModel):
-    title: str
-    content: str
-    image_url: Optional[str] = None
+# class BlogPost(BaseModel):
+#     title: str
+#     content: str = Field(..., min_length=50, max_length=1000)
+#     image_url: Optional[str] = None
+
+
+# my_input = {
+#     "title": "Agentic AI",
+#     "content": "hello sjajhsgjsgjs jagjasgjsajgsjgasjhghjsjhsdgjsgjsgjsjgsjgjsjshjgsjgjsgjsgj",
+#     "image_url": "http",
+# }
+
+# output = BlogPost(**my_input)
+# print(output)
+
+for i in range(10):
+    print(i)
